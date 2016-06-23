4769ea71729089b03a072290f84095ce0193942e
snowman2 <alan.d.snow@usace.army.mil>
2016-06-23 09:59:19 -0500

    exposed stream id and next down id to work with NHDPlus

--- toolbox/scripts/CreateNetworkConnectivityFile.py
+++ toolbox/scripts/CreateNetworkConnectivityFile.py
@@ -19,11 +19,13 @@
                 the required field names in the input drainage line feature class
               Version 2.0, 02/29/2015 - Used numpy array instead to make program faster
                 (adpated from Alan D. Snow, US Army ERDC)
+              Version 2.0, 06/23/2016 - Exposed StreamID and NextDownID to work with NHDPlus
+                (adpated from Alan D. Snow, US Army ERDC)
 -------------------------------------------------------------------------------'''
-import os
 import arcpy
 import csv
-import numpy as NUM
+import numpy as np
+import os
 
 class CreateNetworkConnectivityFile(object):
     def __init__(self):
@@ -44,6 +46,26 @@ class CreateNetworkConnectivityFile(object):
                     direction = 'Input')
         in_drainage_line.filter.list = ['Polyline']
 
+        stream_id = arcpy.Parameter(name = "stream_ID",
+                                 displayName = "Stream ID",
+                                 direction = "Input",
+                                 parameterType = "Required",
+                                 datatype = "Field"
+                                 )
+        stream_id.parameterDependencies = ["in_drainage_line_features"]
+        stream_id.filter.list = ['Short', 'Long']
+        stream_id.value = "HydroID"
+        
+        next_down_id = arcpy.Parameter(name = "nextDownID",
+                                 displayName = "Next Down ID",
+                                 direction = "Input",
+                                 parameterType = "Required",
+                                 datatype = "Field"
+                                 )
+        next_down_id.parameterDependencies = ["in_drainage_line_features"]
+        next_down_id.filter.list = ['Short', 'Long']
+        next_down_id.value = "NextDownID"
+        
         out_csv_file = arcpy.Parameter(
                     displayName = 'Output Network Connectivity File',
                     name = 'out_network_connectivity_file',
@@ -59,6 +81,8 @@ class CreateNetworkConnectivityFile(object):
                     direction = 'Input')
 
         return [in_drainage_line,
+                stream_id,
+                next_down_id,
                 out_csv_file,
                 in_max_nbr_upstream]
 
@@ -70,68 +94,54 @@ class CreateNetworkConnectivityFile(object):
         """Modify the values and properties of parameters before internal
         validation is performed.  This method is called whenever a parameter
         has been changed."""
-        if parameters[1].altered:
-            (dirnm, basenm) = os.path.split(parameters[1].valueAsText)
+        if parameters[3].altered:
+            (dirnm, basenm) = os.path.split(parameters[3].valueAsText)
             if not basenm.endswith(".csv"):
-                parameters[1].value = os.path.join(
+                parameters[3].value = os.path.join(
                     dirnm, "{}.csv".format(basenm))
         else:
-            scratchWorkspace = arcpy.env.scratchWorkspace
-            if not scratchWorkspace:
-                scratchWorkspace = arcpy.env.scratchGDB
-            parameters[1].value = os.path.join(
-                scratchWorkspace, "rapid_connect.csv")
+            parameters[3].value = os.path.join(
+                arcpy.env.scratchFolder, "rapid_connect.csv")
         return
 
     def updateMessages(self, parameters):
         """Modify the messages created by internal validation for each tool
         parameter.  This method is called after internal validation."""
-        try:
-            if parameters[0].altered:
-                field_names = []
-                fields = arcpy.ListFields(parameters[0].valueAsText)
-                for field in fields:
-                    field_names.append(field.baseName.upper())
-                if not ("HYDROID" in field_names and "NEXTDOWNID" in field_names):
-                    parameters[0].setErrorMessage("Input Drainage Line must contain HydroID and NextDownID.")
-        except Exception as e:
-            parameters[0].setErrorMessage(e.message)
-
-        if parameters[2].altered:
-            max_nbr = parameters[2].value
+        if parameters[4].altered:
+            max_nbr = parameters[4].value
             if (max_nbr < 0 or max_nbr > 12):
-                parameters[2].setErrorMessage("Input Maximum Number of Upstreams must be within [1, 12]")
+                parameters[4].setErrorMessage("Input Maximum Number of Upstreams must be within [1, 12]")
         return
 
     def execute(self, parameters, messages):
         """The source code of the tool."""
         in_drainage_line = parameters[0].valueAsText
-        out_csv_file = parameters[1].valueAsText
-        in_max_nbr_upstreams = parameters[2].value
+        stream_id = parameters[1].valueAsText
+        next_down_id = parameters[2].valueAsText
+        out_csv_file = parameters[3].valueAsText
+        in_max_nbr_upstreams = parameters[4].value
 
-        fields = ['HydroID', 'NextDownID']
-        stream_id = fields[0]
-        next_down_id = fields[1]
 
         list_all = []
         max_count_Upstream = 0
         '''The script line below makes sure that rows in the output connectivity
-           file are arranged in ascending order of HydroIDs of stream segements'''
-        np_table = arcpy.da.TableToNumPyArray(in_drainage_line, fields)
-        for hydroid in NUM.sort(np_table[stream_id]):
+        file are arranged in ascending order of HydroIDs of stream segements'''
+        np_table = arcpy.da.TableToNumPyArray(in_drainage_line, [stream_id, next_down_id])
+        for hydroid in np.sort(np_table[stream_id]):
             # find the HydroID of the upstreams
             list_upstreamID = np_table[np_table[next_down_id]==hydroid][stream_id]
             # count the total number of the upstreams
             count_upstream = len(list_upstreamID)
             if count_upstream > max_count_Upstream:
                 max_count_Upstream = count_upstream
+            # replace the nextDownID with 0 if it equals to -1 (no next downstream)
             nextDownID = np_table[np_table[stream_id]==hydroid][next_down_id][0]
 #THIS IS REMOVED DUE TO THE FACT THAT THERE CAN BE STREAMS WITH ID OF ZERO
 #        # replace the nextDownID with 0 if it equals to -1 (no next downstream)
 #        if nextDownID == -1:
 #            nextDownID = 0
              # append the list of Stream HydroID, NextDownID, Count of Upstream ID, and  HydroID of each Upstream into a larger list
-            list_all.append(NUM.concatenate([NUM.array([hydroid,nextDownID,count_upstream]),list_upstreamID]))
+            list_all.append(np.concatenate([np.array([hydroid,nextDownID,count_upstream]),list_upstreamID]))
 
         # If the input maximum number of upstreams is none, the actual max number of upstreams is used
         if in_max_nbr_upstreams == None:
@@ -141,7 +151,7 @@ class CreateNetworkConnectivityFile(object):
             connectwriter = csv.writer(csvfile, dialect='excel')
 
             for row_list in list_all:
-                out = NUM.concatenate([row_list, NUM.array([0 for i in xrange(in_max_nbr_upstreams - row_list[2])])])
+                out = np.concatenate([row_list, np.array([0 for i in xrange(in_max_nbr_upstreams - row_list[2])])])
                 connectwriter.writerow(out.astype(int))
 
 
